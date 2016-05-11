1c679fb0784780f6834ae0b7d44a9b73ae0de11d
Alex <xqhu2008@hotmail.com>
2016-05-11 21:55:45 +0800

    Initial commit

---
+++ app/src/main/java/com/bluehawk/alex/soduku/SodukuMainActivity.java
@@ -0,0 +1,13 @@
+package com.bluehawk.alex.soduku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SodukuMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soduku_main);
+    }
+}
